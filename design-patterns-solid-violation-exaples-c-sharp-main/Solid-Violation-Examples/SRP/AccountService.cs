ef7f710f5c4b11d5d02edb839089c5eabeb7b3a6
HristafulovaOleksandra <hristafulovaoleksandra@gmail.com>
2025-05-13 17:17:41 +0300

    SRP-solved

---
+++ design-patterns-solid-violation-exaples-c-sharp-main/Solid-Violation-Examples/SRP/AccountService.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace Solid_Violation_Examples.SRP
+{
+    public class AccountService
+    {
+        private const string STATEMENT_HEADER = "DATE | AMOUNT | BALANCE";
+        
+
+        private readonly ITransactionRepository _transactionRepository;
+        private readonly Clock _clock;
+        private readonly StatementPrinter _statementPrinter;
+
+        public AccountService(ITransactionRepository transactionRepository, Clock clock, StatementPrinter statementPrinter)
+        {
+            _transactionRepository = transactionRepository;
+            _clock = clock;
+            _statementPrinter = statementPrinter;
+        }
+
+        public void Deposit(int amount) => _transactionRepository.Add(TransactionWith(amount));
+        public void Withdraw(int amount) => _transactionRepository.Add(TransactionWith(-amount));
+        public void PrintStatement()
+        {
+            var t = _transactionRepository.GetAll();
+            _statementPrinter.Print(t);
+        }
+        private Transaction TransactionWith(int amount) => new Transaction(DateOnly.FromDateTime(DateTime.Today), amount);
+    }
+}
